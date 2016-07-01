05bc4c430750d1ac169ba6e55e2b3fc9b7435482
qihao <malkavia@163.com>
2016-07-01 07:24:34 +0000

    界面微调

--- app/src/main/java/com/vito/verticaltab/TabContentAdapter.java
+++ app/src/main/java/com/vito/verticaltab/TabContentAdapter.java
@@ -1,34 +1,29 @@
 package com.vito.verticaltab;
 
 import android.content.Context;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.tonicartos.widget.stickygridheaders.StickyGridHeadersSimpleAdapter;
 import com.vito.verticaltab.bean.ContentBody;
-import com.vito.verticaltab.bean.TabContent;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by pc on 2016/6/30.
  */
 public class TabContentAdapter extends BaseViewAdapter<ContentBody> implements StickyGridHeadersSimpleAdapter {
-    private List<ContentBody> mContentBodyList = new ArrayList<>();
+    private LayoutInflater mInflater;
+
     public TabContentAdapter(Context context, int layoutId) {
         super(context, layoutId);
         mInflater = LayoutInflater.from(context);
     }
-    private LayoutInflater mInflater;
 
     @Override
     public long getHeaderId(int i) {
-        Log.d("qh","i : " + i);
-        Log.d("qh","id : " + mData.get(i).getCategoryId());
         return mData.get(i).getCategoryId();
     }
 
@@ -45,7 +40,6 @@ public class TabContentAdapter extends BaseViewAdapter<ContentBody> implements S
             mHeaderHolder = (HeaderViewHolder) convertView.getTag();
         }
         ContentBody body = getItem(position);
-        Log.d("qh","head position: "+ position);
         mHeaderHolder.mTextView.setText(body.getCategory());
 
         return convertView;
@@ -53,35 +47,36 @@ public class TabContentAdapter extends BaseViewAdapter<ContentBody> implements S
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        ViewHolder viewholder = null;
+        BodyViewHolder viewholder = null;
         if (convertView == null) {
             //获取list_item布局文件的视图
             convertView = super.getView(position, convertView, parent);
             //获取控件对象
-            viewholder = new ViewHolder();
+            viewholder = new BodyViewHolder();
             viewholder.titleView = (TextView) convertView.findViewById(R.id.title);
+            viewholder.imageView = (ImageView) convertView.findViewById(R.id.image);
             convertView.setTag(viewholder);
 
         } else
-            viewholder = (ViewHolder) convertView.getTag();
+            viewholder = (BodyViewHolder) convertView.getTag();
         ContentBody body = getItem(position);
-//
+        String internetUrl = body.getItemImage();
         viewholder.titleView.setText(body.getItemName());
-        Log.d("qh","position" + position );
+        Glide.with(mContext)
+                .load(internetUrl)
+                .placeholder(R.drawable.sq_10)
+                .error(R.drawable.sq_12)
+                .fitCenter()
+                .crossFade()
+                .into(viewholder.imageView);
         return convertView;
     }
 
-    public List<ContentBody> getContentBodyList() {
-        return mContentBodyList;
-    }
-
-    public void setContentBodyList(List<ContentBody> contentBodyList) {
-        mContentBodyList = contentBodyList;
-    }
-
-    public final class ViewHolder {
+    public final class BodyViewHolder {
         public TextView titleView;
+        public ImageView imageView;
     }
+
     public final class HeaderViewHolder {
         public TextView mTextView;
     }
--- app/src/main/java/com/vito/verticaltab/VerticalTabView.java
+++ app/src/main/java/com/vito/verticaltab/VerticalTabView.java
@@ -23,7 +23,6 @@ import com.vito.verticaltab.bean.TabInfo;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.ListIterator;
 import java.util.Map;
 
 /**
@@ -32,7 +31,6 @@ import java.util.Map;
 public class VerticalTabView implements TabHost.TabContentFactory {
     Activity mContext;
     TabContentBean contentBean;
-    private GridView mTabContentGridView;
 
     public VerticalTabView(Activity context) {
         mContext = context;
@@ -50,35 +48,35 @@ public class VerticalTabView implements TabHost.TabContentFactory {
     private void initTabWidget(final TabHost tabHost, TabContentBean contentBean) {
 
         TabWidget tw = tabHost.getTabWidget();
-
         tw.setOrientation(LinearLayout.VERTICAL);
-        Log.d("qh", "size : " + contentBean.getTabInfo().size());
 
         for (int i = 0; i < contentBean.getTabInfo().size(); i++) {
             TabInfo tabInfo = contentBean.getTabInfo().get(i);
-            Log.d("qh", tabInfo.getTabWidget());
             tabHost.addTab(tabHost.newTabSpec(String.valueOf(i)).setIndicator(createIndicatorView(tabHost, tabInfo.getTabWidget(), null)).setContent(this));
         }
-        updateTab(tabHost);
+        updateTabHost(tabHost);
+
         tabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
             @Override
             public void onTabChanged(String tabId) {
-                updateTab(tabHost);
+                updateTabHost(tabHost);
             }
         });
+    }
+    private void updateTabHost(final TabHost tabHost) {
+        updateWidget(tabHost);
+        updateContent(tabHost);
     }
     /**
      * 更新Tab标签的颜色，和字体的颜色
      *
      * @param tabHost
      */
-    private void updateTab(final TabHost tabHost) {
+    private void updateWidget(final TabHost tabHost) {
         for (int i = 0; i < tabHost.getTabWidget().getChildCount(); i++) {
-            View view = tabHost.getTabWidget().getChildAt(i);
             RelativeLayout tabIndicator = (RelativeLayout) tabHost.getTabWidget().getChildAt(i).findViewById(R.id.tab_indicator);
             TextView tv = (TextView) tabHost.getTabWidget().getChildAt(i).findViewById(R.id.title);
             View sideView = tabHost.getTabWidget().getChildAt(i).findViewById(R.id.side_view);
-            tv.setTextSize(16);
             if (tabHost.getCurrentTab() == i) {//选中.
                 tv.setTextColor(mContext.getResources().getColor(R.color.text_selected));
                 sideView.setVisibility(View.VISIBLE);
@@ -91,10 +89,10 @@ public class VerticalTabView implements TabHost.TabContentFactory {
 
             }
         }
-        TabContentAdapter  mTabContentAdapter = new TabContentAdapter(mContext, R.layout.item);
+    }
 
+    private void updateContent(TabHost tabHost) {
         int tabId = Integer.valueOf(tabHost.getCurrentTabTag());
-        Log.d("qh","tab : " + tabId);
         List<TabContent> contents = contentBean.getTabInfo().get(tabId).getTabContent();
         ArrayList<ContentBody> contentBodyList = new ArrayList<>();
         for (TabContent content : contents) {
@@ -104,10 +102,10 @@ public class VerticalTabView implements TabHost.TabContentFactory {
             }
         }
 
+        TabContentAdapter mTabContentAdapter = new TabContentAdapter(mContext, R.layout.item);
         mTabContentAdapter.setData(generateHeaderId(contentBodyList));
-        GridView  mTabContentGridView = (GridView) mContext.findViewById(R.id.asset_grid);
+        GridView mTabContentGridView = (GridView) mContext.findViewById(R.id.asset_grid);
         mTabContentGridView.setAdapter(mTabContentAdapter);
-
     }
 
 
@@ -116,8 +114,7 @@ public class VerticalTabView implements TabHost.TabContentFactory {
         LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
         View tabIndicator = inflater.inflate(R.layout.tab_indicator,
-                tabHost.getTabWidget(), // tab widget is the parent
-                false); // no inflate params
+                tabHost.getTabWidget(),false);
 
         final TextView tv = (TextView) tabIndicator.findViewById(R.id.title);
         tv.setText(label);
@@ -130,22 +127,17 @@ public class VerticalTabView implements TabHost.TabContentFactory {
 
     @Override
     public View createTabContent(String tag) {
-
-
-        final TextView tv = new TextView(mContext);
+        TextView tv = new TextView(mContext);
             return tv;
-
     }
 
-
     private List<ContentBody> generateHeaderId(List<ContentBody> nonHeaderIdList) {
-        Map<String, Integer> mHeaderIdMap = new HashMap<String, Integer>();
+        Map<String, Integer> mHeaderIdMap = new HashMap<>();
         int mHeaderId = 1;
         List<ContentBody> hasHeaderIdList;
         for(ContentBody contentBody : nonHeaderIdList){
 
             String category = contentBody.getCategory();
-            Log.d("qh","category : " + category);
             if(!mHeaderIdMap.containsKey(category)){
                 contentBody.setCategoryId(mHeaderId);
                 mHeaderIdMap.put(category, mHeaderId);
@@ -158,6 +150,5 @@ public class VerticalTabView implements TabHost.TabContentFactory {
 
         return hasHeaderIdList;
     }
-    private void getDateFromJson() {
-    }
+
 }
